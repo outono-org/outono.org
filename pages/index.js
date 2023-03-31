fb9034e8cbdf1eb7ed798c06c54778225805fedd
Malik Piara <malikpiara@gmail.com>
2023-04-01 01:41:38 +0200

    Created profile settings.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import Account from '../components/Account'
 
 const Home = () => {
   const session = useSession()
   const supabase = useSupabaseClient()
 
   return (
-    <div className="container" style={{ borderRadius: '5px', padding: '2rem', backgroundColor: 'white', maxWidth: '420px', margin: '96px auto' }}>
-      <img src="https://user-images.githubusercontent.com/6923650/229231388-8116e682-a217-40d1-a953-4afb544d32f4.jpg" width="45"/>
-      <h2>Entra em Outono.org</h2>
-      <p>Somos empreendedores, programadores e designers que querem partilhar conhecimento com uma nova geração.</p>
+    <>
       {!session ? (
+        <div className="container" style={{ borderRadius: '5px', padding: '2rem', backgroundColor: 'white', maxWidth: '480px', margin: '96px auto' }}>
+        <img src="https://user-images.githubusercontent.com/6923650/229231388-8116e682-a217-40d1-a953-4afb544d32f4.jpg" width="45"/>
+        <h2>Entra em Outono.org</h2>
+        <p>Somos empreendedores, programadores e designers que querem partilhar conhecimento com uma nova geração.</p>
         <Auth
         supabaseClient={supabase}
         appearance={{
@@ -40,10 +42,13 @@ const Home = () => {
         providers={['google', 'github']}
         socialLayout
         />
+        </div>
       ) : (
-        <p>Account page will go here.</p>
+        <div className="container" style={{ padding: '50px 0 100px 0' }}>
+          <Account session={session} />
+        </div>
       )}
-    </div>
+    </>
   )
 }
 
