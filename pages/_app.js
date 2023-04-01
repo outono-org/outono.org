b03b0427cc79a17d266e084d88b4651acda93be6
Malik Piara <malikpiara@gmail.com>
2023-04-01 14:51:47 +0200

    Removed unused css. Attempt to fix mobile rendering.

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,17 @@ import '../global.style.css'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient())
 
   return (
     <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      <Head>
+        <title>Outono</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
       <Component {...pageProps} />
     </SessionContextProvider>
   )
