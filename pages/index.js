86b067b69adbf295ad8b4e10f49fc394c5c10b3f
Malik Piara <malikpiara@gmail.com>
2023-04-01 03:18:00 +0200

    Minor style changes.

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Home = () => {
         <div className="container" style={{ borderRadius: '5px', padding: '2rem', backgroundColor: 'white', maxWidth: '480px', margin: '96px auto' }}>
         <img src="https://user-images.githubusercontent.com/6923650/229231388-8116e682-a217-40d1-a953-4afb544d32f4.jpg" width="45"/>
         <h2>Entra em Outono.org</h2>
-        <p>Somos empreendedores, programadores e designers que querem partilhar conhecimento com uma nova geração.</p>
+        <p style={{ lineHeight: '1.4', color: 'var(--colors-inputLabelText)' }}>Somos empreendedores, programadores e designers cometidos a partilhar conhecimento com uma nova geração.</p>
         <Auth
         supabaseClient={supabase}
         appearance={{
@@ -44,7 +44,9 @@ const Home = () => {
         />
         </div>
       ) : (
-        <div className="container" style={{ padding: '50px 0 100px 0' }}>
+        <div className="container" style={{ borderRadius: '5px', backgroundColor: 'white', padding: '2rem', margin: '96px auto' }}>
+          <h2>Settings</h2>
+          <p style={{ lineHeight: '1.4', color: 'var(--colors-inputLabelText)' }}>Obrigado por estares aqui. Ainda estou a avaliar os próximos passos e em breve voltarei a dar um toque.</p>
           <Account session={session} />
         </div>
       )}
