304ffd67eecbbef41c3ccaec5d67491e0c1880ea
Malik Piara <malikpiara@gmail.com>
2023-04-01 05:58:32 +0200

    Letting the auth component load properly before rendering it.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
@@ -6,15 +7,27 @@ import Account from '../components/Account'
 const Home = () => {
   const session = useSession()
   const supabase = useSupabaseClient()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 500) // Adjust the delay as needed
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
     <>
       {!session ? (
-        <div className="container" style={{ borderRadius: '5px', padding: '2rem', backgroundColor: 'white', maxWidth: '480px', margin: '96px auto' }}>
+        
+        <div className="container" style={{ borderRadius: '5px', padding: '2rem', backgroundColor: 'white', maxWidth: '480px', margin: '60px auto' }}>
         <img src="https://user-images.githubusercontent.com/6923650/229231388-8116e682-a217-40d1-a953-4afb544d32f4.jpg" width="45"/>
         <h2>Entra em Outono.org</h2>
-        <p style={{ lineHeight: '1.4', color: 'var(--colors-inputLabelText)' }}>Somos empreendedores, programadores e designers cometidos a partilhar conhecimento com uma nova geração.</p>
-        <Auth
+        <p style={{ lineHeight: '1.4', color: 'gray' }}>Somos empreendedores, programadores e designers cometidos a partilhar conhecimento com uma nova geração.</p>
+
+        {loading ? (
+          'Loading...'
+        ) : (
+          <>
+            <Auth
         supabaseClient={supabase}
         appearance={{
           theme: ThemeSupa,
@@ -31,8 +44,6 @@ const Home = () => {
                 brandButtonText: 'black',
               },
               space: {
-                spaceSmall: '24px',
-                spaceLarge: '216px',
                 inputPadding: '15px 15px',
                 socialAuthSpacing: '40px',
               }
@@ -42,11 +53,13 @@ const Home = () => {
         providers={['google', 'github']}
         socialLayout
         />
+          </>
+        )}
         </div>
       ) : (
-        <div className="container" style={{ borderRadius: '5px', backgroundColor: 'white', padding: '2rem', margin: '96px auto' }}>
+        <div className="container" style={{ borderRadius: '5px', backgroundColor: 'white', padding: '2rem', margin: '60px auto' }}>
           <h2>Settings</h2>
-          <p style={{ lineHeight: '1.4', color: 'var(--colors-inputLabelText)' }}>Obrigado por estares aqui. Ainda estou a avaliar os próximos passos e em breve voltarei a dar um toque.</p>
+          <p style={{ lineHeight: '1.4', color: 'gray' }}>Obrigado por estares aqui. Ainda estou a avaliar os próximos passos e em breve voltarei a dar um toque.</p>
           <Account session={session} />
         </div>
       )}
