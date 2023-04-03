2a1b7f3fd2c6dab85c7e0c5692fdd6a40a1d038a
Malik Piara <malikpiara@gmail.com>
2023-04-03 23:02:33 +0200

    Made content inside navigation and main scrollable from the container.

--- pages/[user].js
+++ pages/[user].js
@@ -21,14 +21,14 @@ const Home = () => {
       {!session || !user ? (
         <></>
       ) : (
-        <div className='flex'>
-          <div style={{backgroundColor: 'white', width: '30%', borderRight: '1px solid #eaeaea' }}>
+        <div className='flex' style={{height: '100%'}}>
+          <div style={{backgroundColor: 'white', width: '30%', borderRight: '1px solid #eaeaea', overflowY: 'scroll', overflowX: 'hidden' }}>
             <img style={{ position: 'relative', left: '20px', top: '20px'}} src="https://user-images.githubusercontent.com/6923650/229231388-8116e682-a217-40d1-a953-4afb544d32f4.jpg" width="45"/>
             <h2 style={{ position: 'relative', left: '20px', top: '20px'}}>Directório</h2>
             <p style={{position: 'relative', left: '20px', lineHeight:' 1.4', color: 'gray', marginRight: '20px'}}>Somos empreendedores, programadores e designers cometidos a partilhar conhecimento com uma nova geração.</p>
             <UserCards/>
           </div>
-        <div className="container" style={{ height: '100vh', backgroundColor: 'white', padding: '2rem', margin: '0px auto', float: 'right' }}>
+        <div className="container" style={{ height: '100vh', backgroundColor: 'white', padding: '2rem', margin: '0px auto', float: 'right', overflowY: 'auto' }}>
           
           <Profile session={session} username={user} />
         </div>
