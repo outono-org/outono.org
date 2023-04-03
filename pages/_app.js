bcc2c4a45fe5d2584be3f8947fe4372c2350a233
Malik Piara <malikpiara@gmail.com>
2023-04-03 19:11:44 +0200

    Added vercel analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient())
@@ -14,6 +15,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Head>
       <Component {...pageProps} />
+      <Analytics/>
     </SessionContextProvider>
   )
 }
