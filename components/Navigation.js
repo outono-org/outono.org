379ac6af24d38fbb5ac91dd6cef741dd7ea1f6a4
Malik Piara <malikpiara@gmail.com>
2023-05-13 10:31:32 +0200

    Changed sidebar project description.

--- components/Navigation.js
+++ components/Navigation.js
@@ -9,7 +9,7 @@ export default function Navigation() {
               <Image className='logo' src="/logo.jpg" width="48" height="48" quality="100" alt='Logo of Outono'/>
            
               <h2 style={{ position: 'relative', left: '20px', top: '20px'}}>Directório</h2>
-              <p style={{position: 'relative', left: '20px', lineHeight:' 1.4', color: 'gray', marginRight: '20px'}}>Somos empreendedores, programadores e designers cometidos a partilhar conhecimento com uma nova geração.</p>
+              <p style={{position: 'relative', left: '20px', lineHeight:' 1.4', color: 'gray', marginRight: '20px'}}>Somos programadores e designers cometidos a aprender e a partilhar conhecimento.</p>
             </div>
             <UserCards/>
           </header>
